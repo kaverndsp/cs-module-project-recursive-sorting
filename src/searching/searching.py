ccc17087d8b837eff836ac5d938ac29f9404c911
kaverndsp <kaverndsp@users.noreply.github.com>
2020-06-16 17:54:20 -0500

    merge done. working on binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,13 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+    pass
+    # STRETCH: implement an order-agnostic binary search
+    # This version of binary search should correctly find
+    # the target regardless of whether the input array is
+    # sorted in ascending order or in descending order
+    # You can implement this function either recursively
+    # or iteratively
 
 
-# STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
-# the target regardless of whether the input array is
-# sorted in ascending order or in descending order
-# You can implement this function either recursively 
-# or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
-
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,45 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    result = []
+    i = j = 0
+    while i < len(arrA) and j < len(arrB):
+        if arrA[i] < arrB[j]:
+            result.append(arrA[i])
+            i += 1
+        else:
+            result.append(arrB[j])
+            j += 1
+    result += arrA[i:]
+    result += arrB[j:]
 
-    # Your code here
-
-
-    return merged_arr
+    return result
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
-    # Your code here
+    if len(arr) <= 1:
+        return arr
+
+    midpoint = len(arr) // 2
+
+    left = arr[:midpoint]
+    right = arr[midpoint:]
+
+    return merge(merge_sort(left), merge_sort(right))
 
 
-    return arr
+print(merge_sort([1, 5, 4, 2, 8, 4, 7, 6]))
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
-    # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+    pass
